ad26312fe4e4e1a98287f408f9f1645948942c8a
joshneipp <joshneipp@gmail.com>
2017-01-13 06:44:14 -0500

    Only profile one bit of code

--- example1.rb
+++ example1.rb
@@ -2,30 +2,42 @@ require 'profile'
 
 def until_method
   u_counter = 1
-  until u_counter == 7654321 do
+  until u_counter == 87654321 do
     u_counter += 1
   end
 end
 
+
 def while_method
   w_counter = 1
-  while w_counter <= 7654321 do
+  while w_counter <= 87654321 do
     w_counter += 1
   end
 end
 
-until_method
+
 while_method
 
+# start the profiler at the point in the program where you want to begin profiling (duh!)
+Profiler__::start_profile
+
+until_method
+
+Profiler__::stop_profile
+Profiler__::print_profile($stderr)
 
 
 =begin
 %   cumulative   self              self     total
 time   seconds   seconds    calls  ms/call  ms/call  name
-58.00     0.29      0.29        1   290.00   290.00  Object#until_method
-40.00     0.49      0.20        1   200.00   200.00  Object#while_method
-0.00     0.49      0.00        1     0.00     0.00  TracePoint#enable
-0.00     0.49      0.00        2     0.00     0.00  Module#method_added
-0.00     0.49      0.00        1     0.00     0.00  TracePoint#disable
-0.00     0.50      0.00        1     0.00   500.00  #toplevel
+100.00     3.47      3.47        2  1735.00  3470.00  Object#until_method
+0.00     3.47      0.00        3     0.00     0.00  TracePoint#enable
+0.00     3.47      0.00        2     0.00     0.00  TracePoint#disable
+0.00     3.47      0.00        1     0.00  3470.00  #toplevel
+%   cumulative   self              self     total
+time   seconds   seconds    calls  ms/call  ms/call  name
+100.00     3.47      3.47        2  1735.00  3470.00  Object#until_method
+0.00     3.47      0.00        3     0.00     0.00  TracePoint#enable
+0.00     3.47      0.00        2     0.00     0.00  TracePoint#disable
+0.00     3.47      0.00        1     0.00  3470.00  #toplevel
 =end
