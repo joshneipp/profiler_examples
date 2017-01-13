cd7cfcacadc63064cb622b645388e82e026a3645
joshneipp <joshneipp@gmail.com>
2017-01-13 06:48:35 -0500

    If you start profile multiple places, you get data only from the last one

--- example1.rb
+++ example1.rb
@@ -1,12 +1,13 @@
 require 'profile'
 
+
+
 def until_method
   u_counter = 1
   until u_counter == 87654321 do
     u_counter += 1
   end
 end
-
 
 def while_method
   w_counter = 1
@@ -15,6 +16,8 @@ def while_method
   end
 end
 
+# if you start in multiple places, the last instance of start_profile wipes any existing data
+Profiler__::start_profile
 
 while_method
 
@@ -30,14 +33,14 @@ Profiler__::print_profile($stderr)
 =begin
 %   cumulative   self              self     total
 time   seconds   seconds    calls  ms/call  ms/call  name
-100.00     3.47      3.47        2  1735.00  3470.00  Object#until_method
-0.00     3.47      0.00        3     0.00     0.00  TracePoint#enable
-0.00     3.47      0.00        2     0.00     0.00  TracePoint#disable
-0.00     3.47      0.00        1     0.00  3470.00  #toplevel
+100.00     2.61      2.61        3   870.00  2610.00  Object#until_method
+0.00     2.61      0.00        5     0.00     0.00  TracePoint#enable
+0.00     2.61      0.00        3     0.00     0.00  TracePoint#disable
+0.00     2.61      0.00        1     0.00  2610.00  #toplevel
 %   cumulative   self              self     total
 time   seconds   seconds    calls  ms/call  ms/call  name
-100.00     3.47      3.47        2  1735.00  3470.00  Object#until_method
-0.00     3.47      0.00        3     0.00     0.00  TracePoint#enable
-0.00     3.47      0.00        2     0.00     0.00  TracePoint#disable
-0.00     3.47      0.00        1     0.00  3470.00  #toplevel
+100.00     2.61      2.61        3   870.00  2610.00  Object#until_method
+0.00     2.61      0.00        5     0.00     0.00  TracePoint#enable
+0.00     2.61      0.00        3     0.00     0.00  TracePoint#disable
+0.00     2.61      0.00        1     0.00  2610.00  #toplevel
 =end
